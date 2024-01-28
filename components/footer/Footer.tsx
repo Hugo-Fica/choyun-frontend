a1e74061290d013c90c960d35e7a615dbefb5a88
Hugo Fica <hugo.fica.saez@gmail.com>
2024-01-27 23:55:42 -0300

    footer 50%

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className='bg-white flex justify-between px-[300px] bottom-0 w-full h-[50px] items-center fixed'>
+      <span className='text-black text-opacity-35'>
+        © Copyright - Fundación Choyun
+      </span>
+      <span className='text-black text-opacity-35'>Redes sociales</span>
+    </footer>
+  )
+}
