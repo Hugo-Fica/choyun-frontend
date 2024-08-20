048ad033c6e6d15827992941b735a7ca1baece82
DTI-REPO <desarrollotransformacion@gmail.com>
2024-08-20 11:41:45 -0400

    Cambios Responsivos
    Cambios Responsivos

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,10 +1,12 @@
 export const Footer = () => {
   return (
-    <footer className='bg-white flex justify-between px-[300px] items-center h-[50px] '>
-      <span className='text-black text-opacity-35'>
+    <footer className='bg-white flex flex-col sm:flex-row justify-between items-center px-4 sm:px-6 lg:px-24 h-auto sm:h-[50px] py-4 sm:py-0'>
+      <span className='text-black text-opacity-35 text-sm sm:text-base'>
         © Copyright - Fundación Choyun
       </span>
-      <span className='text-black text-opacity-35'>Redes sociales</span>
+      <span className='text-black text-opacity-35 text-sm sm:text-base mt-2 sm:mt-0'>
+        Redes sociales
+      </span>
     </footer>
-  )
+  );
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,17 +1,18 @@
 "use client";
+
 import { useState } from 'react';
 import { PersonIcon, SearchIcon } from '@primer/octicons-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaFacebook, FaInstagram, FaYoutube, FaBars } from 'react-icons/fa';
 
-// Definición del tipo para los enlaces
+
 type LinkType = {
   path: string;
   text: string;
 };
 
-// Array de enlaces con el tipo definido
+
 const links: LinkType[] = [
   { path: '/somos', text: 'Somos' },
   { path: '/presencial', text: 'Presencial' },
@@ -25,7 +26,7 @@ export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className='bg-white px-4 sm:px-6 lg:px-24 fixed top-0 w-full z-10 shadow-md'>
+    <nav className='bg-white px-2 sm:px-4 lg:px-12 fixed top-0 w-full z-10 shadow-md'>
       <div className='flex items-center justify-between h-16'>
         <Link href={'/'} className='flex items-center' legacyBehavior>
           <a>
@@ -33,12 +34,12 @@ export const Navbar = () => {
               className='w-auto pb-[10px]'
               src='/assets/logo_choyun-1.png'
               alt='logo-choyun'
-              width={50}
-              height={50}
+              width={40}
+              height={40} 
             />
           </a>
         </Link>
-        <div className='flex space-x-4 lg:hidden'>
+        <div className='flex space-x-2 lg:hidden'>
           <button
             onClick={() => setIsOpen(!isOpen)}
             className='text-black hover:text-gray-700 focus:outline-none focus:text-gray-700'
@@ -46,70 +47,70 @@ export const Navbar = () => {
             <FaBars size={24} />
           </button>
         </div>
-        <div className='hidden lg:flex lg:items-center lg:space-x-6'>
+        <div className='hidden lg:flex lg:items-center lg:space-x-4'>
           {links.map((link: LinkType) => (
             <Link href={link.path} key={link.text} legacyBehavior>
-              <a className="text-black hover:text-gray-700">{link.text}</a>
+              <a className="text-black hover:text-gray-700 text-sm lg:text-base">{link.text}</a>
             </Link>
           ))}
           <div className='flex items-center'>
-            <SearchIcon size={24} className='fill-black mx-4' />
+            <SearchIcon size={20} className='fill-black mx-2' />
           </div>
           <Link href={'/inicio-sesion'} className='flex items-center' legacyBehavior>
             <a>
-              <PersonIcon size={24} className='fill-black' />
+              <PersonIcon size={20} className='fill-black' />
             </a>
           </Link>
-          <div className='flex space-x-4 ml-4'>
+          <div className='flex space-x-2 ml-2'>
             <Link href='https://web.facebook.com/fundacionchoyun?locale=es_LA' aria-label='Facebook' legacyBehavior>
               <a>
-                <FaFacebook size={24} className='text-black' />
+                <FaFacebook size={20} className='text-black' />
               </a>
             </Link>
             <Link href='https://www.youtube.com/@fundacionchoyun6663' aria-label='YouTube' legacyBehavior>
               <a>
-                <FaYoutube size={24} className='text-black' />
+                <FaYoutube size={20} className='text-black' />
               </a>
             </Link>
             <Link href='https://www.instagram.com/fundacion.choyun/?hl=es' aria-label='Instagram' legacyBehavior>
               <a>
-                <FaInstagram size={24} className='text-black' />
+                <FaInstagram size={20} className='text-black' />
               </a>
             </Link>
           </div>
         </div>
       </div>
-      {/* Menú desplegable para móviles */}
+      {}
       {isOpen && (
         <div className='lg:hidden'>
           <div className='flex flex-col space-y-1 px-2 pt-2 pb-3'>
             {links.map((link: LinkType) => (
               <Link href={link.path} key={link.text} legacyBehavior>
-                <a className="text-black hover:text-gray-700">{link.text}</a>
+                <a className="text-black hover:text-gray-700 text-sm">{link.text}</a>
               </Link>
             ))}
-            <div className='flex items-center justify-between'>
-              <SearchIcon size={24} className='fill-black mx-4' />
+            <div className='flex items-center justify-between mt-2'>
+              <SearchIcon size={20} className='fill-black mx-2' />
               <Link href={'/inicio-sesion'} className='flex items-center' legacyBehavior>
                 <a>
-                  <PersonIcon size={24} className='fill-black' />
+                  <PersonIcon size={20} className='fill-black' />
                 </a>
               </Link>
             </div>
-            <div className='flex space-x-4 mt-4'>
+            <div className='flex space-x-2 mt-4'>
               <Link href='https://web.facebook.com/fundacionchoyun?locale=es_LA' aria-label='Facebook' legacyBehavior>
                 <a>
-                  <FaFacebook size={24} className='text-black' />
+                  <FaFacebook size={20} className='text-black' />
                 </a>
               </Link>
               <Link href='https://www.youtube.com/@fundacionchoyun6663' aria-label='YouTube' legacyBehavior>
                 <a>
-                  <FaYoutube size={24} className='text-black' />
+                  <FaYoutube size={20} className='text-black' />
                 </a>
               </Link>
               <Link href='https://www.instagram.com/fundacion.choyun/?hl=es' aria-label='Instagram' legacyBehavior>
                 <a>
-                  <FaInstagram size={24} className='text-black' />
+                  <FaInstagram size={20} className='text-black' />
                 </a>
               </Link>
             </div>
