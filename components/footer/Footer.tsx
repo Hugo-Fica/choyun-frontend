1845ab5ef3da96461d36fd7c15c3855be11c78c7
DTI-REPO <desarrollotransformacion@gmail.com>
2024-08-20 12:08:13 -0400

    Update Footer.tsx
    footer changes

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,14 +4,14 @@ import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 export const Footer = () => {
   return (
     <footer className='bg-white flex flex-col sm:flex-row justify-between items-center px-4 sm:px-6 lg:px-[300px] py-4 sm:py-0 border-t border-gray-200 h-auto sm:h-[50px]'>
-      <span className='text-black text-opacity-35 text-sm sm:text-base'>
+      <span className='text-black text-opacity-35 text-sm sm:text-base text-center w-full sm:w-auto'>
         © Copyright - Fundación Choyun
       </span>
       <div className='flex items-center mt-2 sm:mt-0'>
-        <span className='text-black text-opacity-35 text-sm sm:text-base sm:mr-4'>
+        <span className='text-black text-opacity-35 text-sm sm:text-base mr-4'>
           Redes sociales
         </span>
-        <div className='flex space-x-4'>
+        <div className='flex space-x-2'>
           <Link href='https://web.facebook.com/fundacionchoyun?locale=es_LA' aria-label='Facebook'>
             <FaFacebook size={20} className='text-black' />
           </Link>
