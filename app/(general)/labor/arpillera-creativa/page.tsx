18f01d0af0867379ced9c2f1e89ac4d88a56185b
Hugo-Fica <hugo.fica.saez@gmail.com>
2024-10-19 21:28:56 -0300

    modificación de causa - labor - sumate responsiva

--- app/(general)/labor/arpillera-creativa/page.tsx
+++ app/(general)/labor/arpillera-creativa/page.tsx
@@ -1,6 +1,6 @@
 export default function ArpilleraCreativaPage() {
   return (
-    <article className='px-[50px] flex flex-col gap-4'>
+    <article className='xs:mt-[100px] lg:px-36 xs:px-4 flex flex-col gap-4 mb-5 sm:px-16'>
       <header className=''>
         <h2 className='font-bold underline'>Arpillera creativa</h2>
       </header>
--- app/(general)/labor/crianza-respetuosa/page.tsx
+++ app/(general)/labor/crianza-respetuosa/page.tsx
@@ -1,6 +1,6 @@
 export default function CrianzaRespetuosaPage() {
   return (
-    <article className='px-[50px] flex flex-col gap-4'>
+    <article className='xs:mt-[100px] lg:px-36 xs:px-4 flex flex-col gap-4 mb-5 sm:px-16'>
       <header className=''>
         <h2 className='font-bold underline'>Círculo de crianza respetuosa</h2>
       </header>
